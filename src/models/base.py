3d613605d1c8793f81f064f0053e9563c892382b
SHAIK BABA MALIK HUSSAIN <babamalik206@gmail.com>
2026-04-28 15:32:11 -0400

    Clean up formatting in base.py
    Remove unnecessary blank line at the top of base.py

--- src/models/base.py
+++ src/models/base.py
@@ -1,7 +1,7 @@
 """
 Base Database Configuration
 SQLAlchemy base class and database session management
-
+"""
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
